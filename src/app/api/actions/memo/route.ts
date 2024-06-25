56301f9b320b5b90d367aad0897e4d2a4f3c22c4
hetdagli234 <daglihet@gmail.com>
2024-06-25 20:53:35 +0530

    Intialize commit

---
+++ src/app/api/actions/memo/route.ts
@@ -0,0 +1,72 @@
+import { ACTIONS_CORS_HEADERS, ActionGetResponse, ActionPostRequest, ActionPostResponse, MEMO_PROGRAM_ID, createPostResponse, parseURL } from "@solana/actions"
+import { ComputeBudgetProgram, Connection, PublicKey, Transaction, TransactionInstruction, clusterApiUrl } from "@solana/web3.js"
+import { getDomainKeysWithReverses } from "@bonfida/spl-name-service"
+export const GET = (req: Request) => {
+    const payload: ActionGetResponse = {
+        icon: new URL("/me.png", new URL(req.url).origin).toString(),
+        label: "Send Fuck you",
+        description: "STFU",
+        title: "Fuck you"
+    }
+
+    return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS
+    })
+}
+
+export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+    try{
+        const body: ActionPostRequest = await req.json();
+
+        let account: PublicKey;
+
+        try{
+            account = new PublicKey(body.account);
+        } catch (err) {
+            return new Response('invalid account provided', {
+                status: 400,
+                headers: ACTIONS_CORS_HEADERS
+            })
+        }
+
+        console.log(`Key verification succeeded ${account}`);
+
+        const transaction = new Transaction();
+
+        transaction.add(
+            ComputeBudgetProgram.setComputeUnitPrice({
+                microLamports: 1000
+            }),
+            new TransactionInstruction({
+                programId: new PublicKey(MEMO_PROGRAM_ID),
+                data: Buffer.from("Fuck you", "utf8"),
+                keys: [],
+            })
+        )
+
+        transaction.feePayer = account;
+        let name;
+        const connection = new Connection(clusterApiUrl("mainnet-beta"));
+        try {
+            name = (await getDomainKeysWithReverses(connection, account))[0].domain;
+        }
+        catch(err){
+            name = "get an SNS domain"
+        }
+
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const payload: ActionPostResponse = await createPostResponse({
+            fields: {
+                transaction,
+                message: `Fuck you ${name}`
+            }
+        })
+
+        return Response.json(payload, { headers: ACTIONS_CORS_HEADERS})
+    } catch(err){
+        return Response.json("unkown error", { status: 400})
+    }
+}
